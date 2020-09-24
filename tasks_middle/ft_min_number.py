17a641d580a334274be749e00fc26bbd98107a84
SumarokovDaniil <dsumrak10@yandex.ru>
2020-09-24 12:03:54 +0300

    123

--- tasks_middle/ft_min_number.py
+++ tasks_middle/ft_min_number.py
@@ -1,7 +1,8 @@
 def ft_min_number(number):
-    min_digit = (number % 10)
+    min_digit = number % 10
     while number > 0:
-        if min_digit < (number % 10):
-            min_digit = (number % 10)
+        digit = number % 10
+        if min_digit > digit:
+            min_digit = digit
         number //= 10
     return min_digit
